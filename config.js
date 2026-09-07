e00f819cc527def6267440f26bded111ac8d688c
ds3hzt <ds3hzt@gmail.com>
2026-09-07 16:35:30 +0900

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzpGk8978VZb9N_OXiWVsOw4eCuCmlpnLT4Ic7XFpqaAwAeK9ZAB_m53lfvZfUZXMQ-kw/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzpGk8978VZb9N_OXiWVsOw4eCuCmIpnLT4lc7XFpqaAwAeK9ZAB_m53IfvZfUZXMQ-kw/exec"
 };
